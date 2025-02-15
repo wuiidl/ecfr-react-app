72c8e9a072e63de0b8579e64843e80fdfea4f189
Walter Gugenberger <walter.gugenberger@gmail.com>
2025-02-15 17:06:47 -0500

    more styling

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,6 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-      crossOrigin="anonymous"
-    />
     <App />
   </StrictMode>,
 )
